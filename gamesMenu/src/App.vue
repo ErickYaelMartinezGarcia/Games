8be6b9c925090727a0efed1141279da80303c796
Erick Yael Martinez Garcia <168610567+ErickYaelMartinezGarcia@users.noreply.github.com>
2024-12-17 23:20:18 +0000

    cambios de primer comiit

---
+++ gamesMenu/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+import board from './components/board.vue'
+</script>
+
+<template>
+ <board></board>
+</template>
+
+<style scoped>
+
+</style>
---
+++ gamesMenu/src/components/board.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+</script>
+
+<template>
+  <div id="board" class="col-12 p-2 row mx-auto">
+    <div class="col-lg-4 border rounded-3 p-2 bgLightBlue">
+      <h3 class="text-center">Instructivo</h3> 
+      <p class="text-wrap">
+      <span class="fw-bold text-primary">PrimerJuego: </span>   
+        es simplemente el bien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual que como al principio
+      </p>
+      <p class="text-wrap">
+      <span class="fw-bold text-primary">PrimerJuego: </span>   
+        es simplemente el bien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual que como al principio
+      </p>
+      <p class="text-wrap">
+      <span class="fw-bold text-primary">PrimerJuego: </span>   
+        es simplemente el bien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual que como al principio
+      </p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
